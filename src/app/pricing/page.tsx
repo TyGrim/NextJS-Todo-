0a1b866d47fb491ae0d84122161cb77d70d5dd35
Tybo <tysongyorfy@gmail.com>
2023-08-12 11:38:22 -0700

    init next.js, prisma, sqlite. removed boilerplate, added pricing page, set up next Link routing

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,13 @@
+//Trip voting page Todo 
+
+export default function Page(){
+    return (
+        <>
+            <header>
+                <h1 className="text-2xl">Vote on our next itinerary</h1>
+            </header>
+        <ul></ul>
+        </>
+
+    )
+}
